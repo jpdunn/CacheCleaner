3084235b5dd7e15e3d05f8b110310cbe76203866
Josh <sleezycheese03@gmail.com>
2017-06-01 21:03:33 +1000

    Modified a bit how the settings are loaded and stored. Also began adding the ability to save the rest of the settings.

--- options.js
+++ options.js
@@ -1,25 +1,43 @@
 var startEnabledEl;
+var appCacheEl;
+var cacheDataEl;
+var cookiesEl;
+var downloadsEl;
+var fileSystemsEl;
+var formDataEl;
+var historyEl;
+var indexedDBEl;
+var passwordsEl;
+var webSQLEl;
+var saveButtonEl;
 
 
 startEnabledEl = document.getElementById("startEnabled");
+appCacheEl = document.getElementById("removeAppCache");
+cacheDataEl = document.getElementById("removeCache");
+cookiesEl = document.getElementById("removeCookies");
+downloadsEl = document.getElementById("removeDownloads");
+fileSystemsEl = document.getElementById("removeFileSystems");
+formDataEl = document.getElementById("removeFormData");
+historyEl = document.getElementById("removeHistory");
+indexedDBEl = document.getElementById("removeIndexedDB");
+passwordsEl = document.getElementById("removePasswords");
+webSQLEl = document.getElementById("removeWebSQL");
+saveButtonEl = document.getElementById("saveButton");
 
-function optionChanged() {
-  var newValue;
 
-
-  newValue = false;
-
-  if (startEnabledEl.checked) {
-    newValue = true;
-  }
-
-  localStorage["startEnabled"] = newValue;
+function saveSettings(){
+  localStorage["startEnabled"] = startEnabledEl.checked;
 }
 
 
 function loadLocalStorageSettings() {
-  startEnabledEl.checked = localStorage["startEnabled"];
+  var checkedValue;
+
+
+  checkedValue = localStorage["startEnabled"];
+  startEnabledEl.checked = checkedValue;
 }
 
-startEnabledEl.addEventListener("change", optionChanged);
+saveButtonEl.addEventListener("click", saveSettings);
 loadLocalStorageSettings();
